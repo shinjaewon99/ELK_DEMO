cece74963d2600dfcd50b7be9906991e2ffb9775
shinjaewon99 <tlswodnjs99@naver.com>
2024-11-07 22:04:30 +0900

    feat : api 엔드포인트 추가

---
+++ src/main/java/com/demo/elk/controller/elkController.java
@@ -0,0 +1,15 @@
+package com.demo.elk.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class elkController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello ELK";
+    }
+}
