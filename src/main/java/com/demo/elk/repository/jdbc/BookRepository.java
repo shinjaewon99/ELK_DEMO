d46053f45712620f2d4390a2780c7bdf0337251d
shinjaewon99 <tlswodnjs99@naver.com>
2024-11-27 23:05:52 +0900

    feat : 벌크 insert를 위한 파일 추가

--- src/main/java/com/demo/elk/repository/jdbc/BookRepository.java
+++ src/main/java/com/demo/elk/repository/jdbc/BookRepository.java
@@ -2,7 +2,8 @@ package com.demo.elk.repository.jdbc;
 
 import com.demo.elk.domain.Book;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,27 +12,25 @@ import java.sql.PreparedStatement;
 import java.util.List;
 
 @Repository
-
+@RequiredArgsConstructor
 public class BookRepository {
 
     private final JdbcTemplate jdbcTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(BookRepository.class);
 
-    @Autowired
-    public BookRepository(final JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
 
     @Transactional
-    public void saveAll(List<Book> books) {
-        String sql = "INSERT INTO BOOK (bk_nm, aut_nm, pblshr) VALUES (?, ?, ?)";
+    public void saveAll(List<Book> bookEntities) {
+        String sql = "INSERT INTO book (bk_nm, aut_nm, pblshr) VALUES (?, ?, ?)";
 
         jdbcTemplate.batchUpdate(sql,
-                books,
-                books.size(),
+                bookEntities,
+                100,
                 (PreparedStatement ps, Book book) -> {
                     ps.setString(1, book.getBookName());
                     ps.setString(2, book.getAuthorName());
                     ps.setString(3, book.getPblshr());
                 });
+        logger.info("Successfully saved {} books", bookEntities.size());
     }
 }
